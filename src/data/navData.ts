88db8d7ac676849d5d96a181e9a8a79a2f4b03ba
RelancioBorgesDev <relancio.dev@gmail.com>
2024-03-19 12:45:17 -0300

    chore: update style footer and nav

---
+++ src/data/navData.ts
@@ -0,0 +1,15 @@
+type NavDataType = {
+  name: string;
+  path: string;
+};
+
+export const navData: NavDataType[] = [
+  {
+    name: "About",
+    path: "/about/",
+  },
+  {
+    name: "Blog",
+    path: "/blog/",
+  },
+];
